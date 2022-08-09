5c211a0e9ba1c1c7a580d99189de40dd5a476257
shaantanu9 <shantanubombatkar2@gmail.com>
2022-08-09 17:57:50 +0530

    updated

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import RoutesLink from './router/RoutesLink' //importing the Routes component
+function App() {
+  return (
+    <div className='App'>
+    <h1 className='text-3xl font-bold underline animate-bounce hover:bg-red-500'>
+      Hello world! This is Shantanu
+    </h1>
+    <RoutesLink/>
+    </div>
+  );
+}
+
+export default App;
